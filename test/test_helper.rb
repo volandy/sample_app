4c288b69cd80966e940272adb2e1497ad1818e2a
volandy <volandy14@gmail.com>
2016-03-04 14:00:11 +0200

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,14 +17,14 @@ class ActiveSupport::TestCase
 
   # Logs in a test user.
   def log_in_as(user, options = {})
-  	password = options[:password] || 'password'
+  	password    = options[:password] || 'password'
     remember_me = options[:remember_me] || '1'
   	if integration_test?
   		post login_path, session: { email: user.email,
-  									password: password,
-  									remember_me: remember_me }
+  									              password: password,
+  									              remember_me: remember_me }
   	else
-  		session[:user_id] = user_id
+  		session[:user_id] = user.id
   	end
   end
 
